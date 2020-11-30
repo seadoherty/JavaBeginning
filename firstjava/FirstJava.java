ea154c0ebc11d96f13459e7fbbea5db74295b5b8
Administrator <admin@example.com>
2020-11-30 16:08:21 -0500

    first commit

---
+++ firstjava/FirstJava.java
@@ -0,0 +1,9 @@
+package firstjava;
+
+public class FirstJava{
+	public static void main (String[] args){
+		System.out.println("My name is Seamus");
+		System.out.println("I am 39 years old");
+		System.out.println("My hometown is Manassas, VA");
+	}
+}
---
+++ pyth/Test.java
@@ -0,0 +1,9 @@
+package pyth;
+
+public class Test{
+	public static void main(String args[]) {
+		Pythagorean py = new Pythagorean();
+		double answer = py.calculateHypotenuse(4,5);
+		System.out.println(answer);
+	}
+}
